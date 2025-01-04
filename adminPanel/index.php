f6304e17359392f640c229ba942d8f048bddb2b2
bornohin2019 <bornohin2019@gmail.com>
2025-01-04 13:04:04 +0600

    add admin panel

--- adminPanel/index.php
+++ adminPanel/index.php
@@ -1,3 +1,23 @@
+<?php
+session_start();
+
+
+// if (!isset($_SESSION["mySession"])) {
+//     header('location:logout.php');
+// }
+// require_once('myClass.php');
+if(isset($_POST['btnSubmit'])){
+$name=$_POST['name'];
+$email=$_POST['email'];
+$username=$_POST['username'];
+$password=$_POST['password'];
+
+$myObj= new Trinee($name,$email,$username,$password);
+$myObj->save();
+}
+?>
+
+        
         <?php 
         include("./pages/common_pages/header.php");
         ?>
